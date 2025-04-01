edc9ce6ecb29d3d67135e072267a516cd4df1d79
Nestor Villa <nestorvillap@gmail.com>
2025-04-01 21:22:44 +0200

    style: update SubmitButton styles for improved visibility

--- src/components/form/SubmitButton.jsx
+++ src/components/form/SubmitButton.jsx
@@ -1,7 +1,7 @@
 const SubmitButton = ({ isSubmitting }) => (
   <button
     type="submit"
-    className={`w-56 border-2 px-4 py-2 rounded-xl cursor-pointer hover:bg-black hover:text-white mt-5 ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''}`}
+    className={`w-56 text-white bg-primary px-4 py-2 rounded-xl cursor-pointer  mt-5 ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''}`}
     disabled={isSubmitting}
   >
     {isSubmitting ? 'Enviando...' : 'Enviar'}
